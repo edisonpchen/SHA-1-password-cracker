9ee29e56ae50279e24b2d15b0af99c58c41282de
Edison Chen <30671669+edisonpchen@users.noreply.github.com>
2022-10-27 15:15:17 -0700

    Reading in Password List

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,8 @@
 import hashlib
 
-def crack_sha1_hash(hash):
+def crack_sha1_hash(hash, use_salts = False):
+    pass_file = open('top-10000-passwords.txt', 'r')
+    pass_list = pass_file.readlines()
+    pass_file.close()
+    pass_list = [s.replace('\n', '') for s in pass_list]
     
