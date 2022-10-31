f10cd95dfad7e6638832c13e895222bc393c4d33
Edison Chen <30671669+edisonpchen@users.noreply.github.com>
2022-10-30 23:32:09 -0700

    Check hashed passwords if not using salts

--- password_cracker.py
+++ password_cracker.py
@@ -5,4 +5,10 @@ def crack_sha1_hash(hash, use_salts = False):
     pass_list = pass_file.readlines()
     pass_file.close()
     pass_list = [s.replace('\n', '') for s in pass_list]
-    
+    if not use_salts:
+        for i in range(len(pass_list)):
+            password = pass_list[i]
+            digest = hashlib.sha1(password.encode()).hexdigest()
+            if digest == hash:
+                return password
+        return 'PASSWORD NOT IN DATABASE'
