9b5a520679c685d151c84aaec96381ede23ab434
Edison Chen <30671669+edisonpchen@users.noreply.github.com>
2022-11-05 23:00:24 -0700

    Completed password_cracker.py. Passes all tests and now returns plaintexts of hashed passwords

--- password_cracker.py
+++ password_cracker.py
@@ -16,3 +16,13 @@ def crack_sha1_hash(hash, use_salts = False):
         salt_file = open('known-salts.txt', 'r')
         salt_list = salt_file.readlines()
         salt_list = [s.replace('\n', '') for s in salt_list]
+        for i in range(len(salt_list)):
+            salt = salt_list[i]
+            for j in range(len(pass_list)):
+                password = pass_list[j]
+                digest1 = hashlib.sha1((password + salt).encode()).hexdigest()
+                digest2 = hashlib.sha1((salt + password + salt).encode()).hexdigest()
+                digest3 = hashlib.sha1((salt + password).encode()).hexdigest()
+                if digest1 == hash or digest2 == hash or digest3 == hash:
+                    return password
+        return 'PASSWORD NOT IN DATABASE'
